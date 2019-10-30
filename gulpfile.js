88f74684b2dd2f1bf9b7c765f8f8c34b7a97279c
Martin Gay <martingay1975@gmail.com>
2019-10-30 22:00:23 +0000

    adding json

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var debug = require("gulp-debug");
+var watch = require("gulp-watch");
+
+var sassConfig = {
+	inputDirectory: "Content/main.scss",
+	outputDirectory: "Content",
+	options: {
+		outputStyle: "expanded"
+	}
+};
+
+var buildcss = function() {
+	return gulp
+		.src(sassConfig.inputDirectory)
+		.pipe(sass(sassConfig.options).on("error", sass.logError))
+		.pipe(gulp.dest(sassConfig.outputDirectory));
+}
+
+gulp.task("build-css", buildcss);
+
+gulp.task("watch", function () {
+	watch("Scripts/**/**/*.scss")
+		.on("change", function (file) {
+			buildcss();
+			console.log(`Compiled Sass ${file}`);
+		});
+});
+
