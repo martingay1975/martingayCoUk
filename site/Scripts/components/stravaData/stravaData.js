da923738841e2e331827008a1fc943dd3b65235e
Martin Gay <martingay1975@gmail.com>
2019-12-18 18:20:02 +0000

    Strava Data progression 2

--- site/Scripts/components/stravaData/stravaData.js
+++ site/Scripts/components/stravaData/stravaData.js
@@ -3,25 +3,164 @@ define(["jquery", "gmapLoader", "text!./stravaData.html"], function ($, gmapLoad
 
 	"use strict";
 
-    var GoogleMap = function() {
+    var StavaActivitiesLoader;
 
-        var drawLine = function() {
-            
+    var extendJQuery = function() {
+        if (typeof jQuery.when.all === 'undefined') {
+            jQuery.when.all = function (deferreds) {
+                return $.Deferred(function (def) {
+                    $.when.apply(jQuery, deferreds).then(
+                        function () {
+                            def.resolveWith(this, [Array.prototype.slice.call(arguments)]);
+                        },
+                        function () {
+                            def.rejectWith(this, [Array.prototype.slice.call(arguments)]);
+                        });
+                });
+            }
+        }
+    };
+
+    StavaActivitiesLoader = function() {
+        
+        extendJQuery();
+        var self = this;
+
+        // Helper to lazily get JSON entries,
+        var getJSONAsync = function(activityId, jsonPath) {
+
+            var deferred = new $.Deferred();
+
+            var cacheValue = window.localStorage.getItem(activityId);
+            if (cacheValue != null) {
+                var ret = JSON.parse(cacheValue);
+                deferred.resolve(ret);
+            } 
+            else {
+                var promise = $.getJSON(jsonPath);
+                promise.done(function(response) {
+                    var stringJson = JSON.stringify(response);
+                    window.localStorage.setItem(activityId, stringJson);
+                    deferred.resolve(response);
+                });
+
+                promise.fail(function (msg) {
+                    deferred.reject(msg);
+                });
+            }
+
+            return deferred.promise();
         };
 
-        var promise = gmapLoader({key: "AIzaSyBYemck0mQmHpfMmqrxgERNPaYZ99RbiuQ"})
-        promise.then(function(googleMaps) {
+        this.getActivitiesAsync = function(drawActivity)
+        {
+            var activityIds = ["2534549498", "2540249862"]
+            var promises = [];
+
+            for (var index=0; index<activityIds.length; index++)
+            {
+                var activityId = activityIds[index];
+                var promise = self.getActivityAsync(activityId);
+                promises.push(promise);
+            }
+
+            var overallPromise = $.when.all(promises).then(function(activities) {
+                for (var index=0; index < activities.length; index++)
+                {
+                    var activity = activities[index];
+                    drawActivity(activity);
+                }
+            });
+
+            return overallPromise;
+        };
+        
+        this.getActivityAsync = function(activityId) {
+            var jsonFilePath = "/res/strava/Run/" + activityId + ".json";
+            var activityPromise = getJSONAsync(activityId, jsonFilePath);
+            return activityPromise;
+        };
+    };
+
+    var GMapLngLat = function (lng, lat) {
+        this.lng = lng;
+        this.lat = lat;
+    };
+
+    var GoogleMap = function() {
+
+        var self = this;
+
+        this.drawLines = function(activity) {
+          var latLngArray = activity.Latlng.data;
+          var gMapLngLatArray = [];
+          for (var index = 0; index < latLngArray.length; index ++)
+          {
+              var latlng = latLngArray[index];
+              var lat = latlng[0];
+              var lng = latlng[1];
+              gMapLngLatArray.push(new google.maps.LatLng(lat, lng));
+          }
+
+          self.drawPolyline(gMapLngLatArray, 3);
+        };
+
+        this.drawPolyline = function(lngLatArray, activityCount)
+        {
+            var hotOpacity = 0.2 * Math.pow(activityCount, -0.3);
+            var hot = new google.maps.Polyline({
+                clickable: true,
+                map: self.map,
+                strokeColor: "#FFFF00",
+                strokeWeight: 5,
+                strokeOpacity: hotOpacity,
+                zIndex: 4,
+                path: lngLatArray
+            });
+
+            var medOpacity = 0.54 * Math.pow(activityCount, -0.292);
+            var medium = new google.maps.Polyline({
+                clickable: true,
+                map: self.map,
+                strokeColor: "#FF0000",
+                strokeWeight: 4,
+                strokeOpacity: medOpacity,
+                zIndex: 3,
+                path: path
+            });
+            var cold = new google.maps.Polyline({
+                clickable: true,
+                map: self.map,
+                strokeColor: "#0000FF",
+                strokeWeight: 3,
+                strokeOpacity: 0.6,
+                zIndex: 2,
+                path: path
+            });
+        }
+
+        this.map = null;
+
+        var promiseMapLoader = gmapLoader({key: "AIzaSyBYemck0mQmHpfMmqrxgERNPaYZ99RbiuQ"})
+        promiseMapLoader.then(function(googleMaps) {
             var mapProp= {
                 center:new google.maps.LatLng(51.3132412,-0.7379628),
                 zoom:12,
             };
     
             var div = document.getElementById("googleMap");
-            var map = new googleMaps.Map(div, mapProp);
+            self.map = new googleMaps.Map(div, mapProp);
+
+            var stavaActivitiesLoader = new StavaActivitiesLoader();
+            var promise = stavaActivitiesLoader.getActivitiesAsync(self.drawLines);
+            promise.done(function() {
+                console.log("Got all activities");
+            });
+    
         });
 
-        
     };
+
 
 	// var StravaDataComponentViewModel = function (params) {
 
